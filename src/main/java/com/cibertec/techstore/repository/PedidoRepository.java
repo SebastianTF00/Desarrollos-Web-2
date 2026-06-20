747b9c58de97b6f2a591c2f07e57b85131f80cf9
SebastianTF00 <sebastian.talavera.f@hotmail.com>
2026-06-20 18:09:52 -0500

    PedidoRepository y UsuarioRepository

--- src/main/java/com/cibertec/techstore/repository/PedidoRepository.java
+++ src/main/java/com/cibertec/techstore/repository/PedidoRepository.java
@@ -1,5 +1,10 @@
 package com.cibertec.techstore.repository;
 
-public class PedidoRepository {
-    
+import com.cibertec.techstore.model.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+    // JpaRepository ya te incluye: findAll(), save(), findById(), deleteById()
 }
--- src/main/java/com/cibertec/techstore/repository/UsuarioRepository.java
+++ src/main/java/com/cibertec/techstore/repository/UsuarioRepository.java
@@ -1,5 +1,11 @@
 package com.cibertec.techstore.repository;
 
-public class UsuarioRepository {
-    
+import com.cibertec.techstore.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    // Listo, JpaRepository nos regala todos los métodos CRUD automáticamente
+    Usuario findByCorreo(String correo);
 }
